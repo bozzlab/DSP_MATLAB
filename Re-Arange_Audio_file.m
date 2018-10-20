6fbe38f9ceae669a0ad89547231a81f35f483a7e
bozzlab <41643434+bozzlab@users.noreply.github.com>
2018-10-20 23:24:10 +0700

    Create Re-Arange_Audio_file.m

---
+++ Re-Arange_Audio_file.m
@@ -0,0 +1,20 @@
+function [Y,f]= process_rec(filepath,flag_aw) 
+          
+if nargin < 2
+    flag_aw=0;
+end   
+
+[y,fs] = audioread([filepath]);
+y=y(1:(fs*60));
+
+y_new = reshape(y,[3*fs,numel(y)/(3*fs)]);
+% t=(0:(3*fs-1))/fs;
+y_new_avg=mean(y_new,2);
+if flag_aw 
+    [b,a]= Aweight(fs);
+    y_new_avg=filter(b,a,y_new_avg);
+ 
+end
+
+   Y=fft(y_new_avg);
+ f=(0:(numel(y_new_avg)-1))*fs/numel(y_new_avg);
